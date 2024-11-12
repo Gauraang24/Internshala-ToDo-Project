8ebfd72ebc5a65006b6e00ad6e60be1922c91e47
gaurang.more <gaurang.more@sunday.tech>
2024-11-12 22:31:57 +0530

    Initial File Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import './index.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-5">
+      Hello, Tailwind!
+    </div>
+  );
+}
+
+
+export default App
